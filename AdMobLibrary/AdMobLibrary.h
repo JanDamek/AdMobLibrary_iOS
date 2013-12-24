5d12b7079092f9cc826b6dde4ce810fcc29b629b
Jan Damek <damekjan74@gmail.com>
2013-12-24 12:01:21 +0100

    Initial Commit

---
+++ AdMobLibrary/AdMobLibrary.h
@@ -0,0 +1,13 @@
+//
+//  AdMobLibrary.h
+//  AdMobLibrary
+//
+//  Created by Jan Damek on 24.12.13.
+//  Copyright (c) 2013 Jan Damek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdMobLibrary : NSObject
+
+@end
---
+++ AdMobLibrary/AdMobLibrary.m
@@ -0,0 +1,13 @@
+//
+//  AdMobLibrary.m
+//  AdMobLibrary
+//
+//  Created by Jan Damek on 24.12.13.
+//  Copyright (c) 2013 Jan Damek. All rights reserved.
+//
+
+#import "AdMobLibrary.h"
+
+@implementation AdMobLibrary
+
+@end
